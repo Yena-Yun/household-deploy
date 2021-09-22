8fc49b357d9f311a4a0e606f76918ad4e2925510
Yena-Yun <cheryleduart@gmail.com>
2021-09-22 20:29:40 +0900

    Updates

--- src/lib/formatMoney.js
+++
@@ -1,5 +0,0 @@
-// 어떤 함수를 처리해서 값을 반환(return)하는 게 아니라
-// 단순히 들어온 숫자값을 문자열로 바꿔서 쉼표 찍어주는 기능만 하는 컴포넌트
-//  => return문 없고, 즉 함수가 아니므로 {}를 쓰지 않는다.
-const formatMoney = (money) => money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-export default formatMoney;
